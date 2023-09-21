4f3a3185bbb292d1efad502af825fd899e5d2640
john makateto <jmakateto@gmail.com>
2023-09-21 13:22:20 +0300

    made changes

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -2,12 +2,19 @@ import requests
 import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.text
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        return json.loads(response_body)
+
+
+if __name__ == "__main__":
+    get_requester = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+    json_data = get_requester.load_json()
+    print(json_data)
